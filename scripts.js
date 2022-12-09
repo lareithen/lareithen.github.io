57c7c13b5e520023baf6fe17d1e2bca7b53a56fb
emirhan <72984140+lareithen@users.noreply.github.com>
2022-12-10 00:57:58 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+window.onload = function hello_world(){
+    const today = new Date();
+    const birthday = new Date(2005, 12, 9);
+    document.getElementById('age').innerHTML = Math.round(Math.abs((today - birthday) / (24 * 60 * 60 * 1000)));
+};
+
 function key_press_func(e){
     var keynum;
 
@@ -7,13 +13,13 @@ function key_press_func(e){
         keynum = e.which;
     }
 
-    document.getElementById('input').innerHTML += String.fromCharCode(keynum)
-}
+    document.getElementById('input').innerHTML += String.fromCharCode(keynum);
+};
 
 window.addEventListener('keydown', function(event) {
     const key = event.key;
     if (key === "Backspace") {
-        let str = document.getElementById('input')
-        str.innerHTML = str.innerText.slice(0, -1)
+        let str = document.getElementById('input');
+        str.innerHTML = str.innerText.slice(0, -1);
     }
 });
